896f8640bffaca1a02353313ea57245bf5f0b29c
Varun Boppudi <varunboppudi@Varuns-MacBook-Air.local>
2025-07-24 21:31:50 +0530

    Updated Version

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const name = document.getElementById('name').value.trim();
             const email = document.getElementById('email').value.trim();
-            const subject = document.getElementById('subject').value.trim();
+            //const subject = document.getElementById('subject').value.trim();
             const message = document.getElementById('message').value.trim();
 
             formMessage.textContent = '';
